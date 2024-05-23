43a282a4fe3cf0a10d11a7a086f753c2eabd4b45
vDang <vuongdang9@gmail.com>
2024-05-23 09:13:46 +0200

    Remove presence.nvim plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -11,13 +11,6 @@ return {
     opts = {},
   },
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
   -- == Examples of Overriding Plugins ==
 
   -- customize alpha options
